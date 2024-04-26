165f66ed00d9f6a4855ae2a41dd09723288de8d1
dani kais <109339926+danikais@users.noreply.github.com>
2024-04-26 13:43:11 +0300

    Update dbconnect.php

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,6 +1,15 @@
 <?php
-$DBConnect = mysqli_connect("localhost", "root", "", "cinamadb");
-if (!$DBConnect)
-		die("<p>The server is not available.</p>");
-//echo"connected";
+// Database configuration
+$servername = "db-service"; // For Docker and Minikube, use the appropriate IP or hostname
+$username = "admin";
+$password = "root"; // Assuming the password is 'root'
+$dbname = "myclinic"; // Change this to your database name if different
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
