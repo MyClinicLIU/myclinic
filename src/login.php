1b8c966f81cb380e3befe10c6168bcc5d2d37b4c
dani kais <109339926+danikais@users.noreply.github.com>
2024-05-08 14:54:21 +0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -6,7 +6,7 @@ if(isset($_SESSION["valid_login"]) && $_SESSION["valid_login"]){
    header("Location: index.php");
 }
 
-include_once "db_connection.php";
+include_once "dbconnection.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])){
 
