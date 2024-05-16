1a2440f35fd56cfae1d91184182931e60cb31da8
dani kais <109339926+danikais@users.noreply.github.com>
2024-05-16 10:11:28 +0300

    Update addmeeting.php

--- src/addmeeting.php
+++ src/addmeeting.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION["valid_login"]) || !$_SESSION["valid_login"] || $_SESSION["a
     header("Location: index.php");
  }
 
-include_once "db_connection.php";
+include_once "dbconnect.php";
 
 $doctor_id = $_SESSION["id"];
 
